0847df2f14ab955be527a52f48b372332eb0a27f
MatheusHenrique60 <105647989+MatheusHenrique60@users.noreply.github.com>
2022-07-24 16:56:48 -0300

    Add files via upload

---
+++ exercicio6/src/test/java/web/exercicio6/Exercicio6ApplicationTests.java
@@ -0,0 +1,13 @@
+package web.exercicio6;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Exercicio6ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
